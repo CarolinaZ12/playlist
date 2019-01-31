71e8d0d2cd71d6620f6537a3f3ce6db1c6c8d5fa
Carolina E. Zhagnay <carolinazhagnay01@gmail.com>
2019-01-31 22:22:59 +0000

    finished

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@
 var songs = ["Stolen Dance","My Blood","Waiting for Love","Paradise","Sicko Mode","Nice for What"];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-var images_links= ["https://www.youtube.com/watch?v=N2EVWDaGI4I&start_radio=1&list=RDMMN2EVWDaGI4I","https://www.youtube.com/watch?v=0a2ePzVCKuk&index=16&list=RDMMN2EVWDaGI4I","https://www.youtube.com/watch?v=jGkjPTJoxNo&list=RDMMN2EVWDaGI4I&index=24","https://www.youtube.com/watch?v=Q0TEUMPIhk8&list=RDMMN2EVWDaGI4I&index=30","https://www.youtube.com/watch?v=gRctGzbk6O4","https://www.youtube.com/watch?v=d-JBBNg8YKs"];
+var songsLinks= ["https://www.youtube.com/watch?v=N2EVWDaGI4I&start_radio=1&list=RDMMN2EVWDaGI4I","https://www.youtube.com/watch?v=0a2ePzVCKuk&index=16&list=RDMMN2EVWDaGI4I","https://www.youtube.com/watch?v=jGkjPTJoxNo&list=RDMMN2EVWDaGI4I&index=24","https://www.youtube.com/watch?v=Q0TEUMPIhk8&list=RDMMN2EVWDaGI4I&index=30","https://www.youtube.com/watch?v=gRctGzbk6O4","https://www.youtube.com/watch?v=d-JBBNg8YKs"];
 var artists = ["Milky Chance","Twenty One Pilots","Avicii","Coldplay","Travis Scott", "Drake"];
-var songLinks = ["https://images-na.ssl-images-amazon.com/images/I/51P6txePs6L._SY355_.jpg","https://www.101x.com/sites/g/files/exi636/f/styles/large_730/public/article-images-featured/maxresdefault_1_0.jpg?itok=efsJxUng","https://upload.wikimedia.org/wikipedia/en/thumb/8/81/Avicii%27s_Waiting_For_Love%2C_Cover_Artwork.png/220px-Avicii%27s_Waiting_For_Love%2C_Cover_Artwork.png","https://upload.wikimedia.org/wikipedia/en/thumb/2/22/Coldplay_-_Paradise.JPG/220px-Coldplay_-_Paradise.JPG","https://i.ytimg.com/vi/d-JBBNg8YKs/maxresdefault.jpg","https://upload.wikimedia.org/wikipedia/en/thumb/7/75/Drake_Nice_for_What.png/220px-Drake_Nice_for_What.png"];
+var imagesLinks= ["https://assets.audiomack.com/alejob333/milky-chance-stolen-dance-album-version-275-275-1545684324.jpg","https://www.101x.com/sites/g/files/exi636/f/styles/large_730/public/article-images-featured/maxresdefault_1_0.jpg?itok=efsJxUng","https://upload.wikimedia.org/wikipedia/en/thumb/8/81/Avicii%27s_Waiting_For_Love%2C_Cover_Artwork.png/220px-Avicii%27s_Waiting_For_Love%2C_Cover_Artwork.png","https://upload.wikimedia.org/wikipedia/en/thumb/2/22/Coldplay_-_Paradise.JPG/220px-Coldplay_-_Paradise.JPG","https://i.ytimg.com/vi/d-JBBNg8YKs/maxresdefault.jpg","https://upload.wikimedia.org/wikipedia/en/thumb/7/75/Drake_Nice_for_What.png/220px-Drake_Nice_for_What.png"];
 var lengths=["5 min 13 sec","3 min 50 sec","3 min 50 sec","4 min 38 sec","5 min 14 sec","3 min 31 sec"];
     // BELOW Use forEach Loop to display the data from each of your array's in the correct div
     
@@ -15,7 +15,7 @@ function displayList(){
     $("#songs").append("<li>"+element+"</li>");
     
 });
-images_links.forEach(function(element2){
+imagesLinks.forEach(function(element2){
     $("#images").append("<img src="+ element2+">");
     
 });
@@ -27,7 +27,7 @@ lengths.forEach(function(element4){
     $("#lengths").append("<li>"+element4+"</li>");
     
 });
-songLinks.forEach(function(element5){
+songsLinks.forEach(function(element5){
     $("#links").append("<a href="+element5+">"+"Listen to this bop!"+"</a");
     
 });
@@ -50,9 +50,9 @@ function addSongInfo(){
     var userInput3 = $("#length").val();
          lengths.push(userInput3);
     var userInput4 = $("#image").val();
-         images_links.push(userInput4);
+         imagesLinks.push(userInput4);
     var userInput5 = $("#link").val();
-         songLinks.push(userInput5);
+         songsLinks.push(userInput5);
 
 }
 
